32924f85e994fa9171b5b8b47a25af5ae67e1cb9
NyHagan <nanaohagan@gmail.com>
2021-12-19 14:12:28 +0000

    if statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,7 +84,7 @@ function calculateCorrectAnswer(){
     } else if(operator === "-"){
         return [operand1 - operand2, "subtract"];
     } else if(operator === "/"){
-        return [operand1 / operand2, "division"];
+        return [parseInt(operand1 / operand2), "division"];
     } else{
         alert(`unimplemented operator ${operator}`);
         throw (`unimplemented operator ${operator}. Aborting!`);
@@ -127,4 +127,13 @@ function displayDivisionQuestion(operand1, operand2){
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "/";
+
+    if(operand2 >= operand1){
+        document.getElementById('operand2').textContent = operand1;
+        document.getElementById('operand1').textContent = operand2; 
+        operand1++;
+    }
+
+    
+    
 }
